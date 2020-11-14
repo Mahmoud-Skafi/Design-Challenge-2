b09c553a8e3e92eb0f8719eba0b9c698c08451c9
mahmoud skafi <mahmoudskafi277@gmail.com>
2020-11-14 16:37:02 +0200

    add animation

--- js/app.js
+++ js/app.js
@@ -84,18 +84,68 @@ $("#search-candidate-btn").click(() => {
     $(".search-job-form").css("display", "none");
 });
 var header = document.getElementById("jb-navbar");
-window.addEventListener('scroll', function () {
-    header.classList.add('fixed-top', window.scrollY > 200);
-});
+// window.addEventListener('scroll', function () {
+//     header.classList.add('sticky', window.scrollY > 100000);
+//     console.log("skafi")
+// });
 
 var prevScrollpos = window.pageYOffset;
+
 window.onscroll = function () {
     var currentScrollPos = window.pageYOffset;
-    if (prevScrollpos > currentScrollPos) {
-
-        header.classList.add('sticky');
-    } else {
+    if (currentScrollPos <= 0) {
         header.classList.remove('sticky');
     }
+    else if (currentScrollPos > 360) {
+        header.classList.add('sticky');
+    }
+    // if (prevScrollpos > currentScrollPos) {
+
+    //     header.classList.add('sticky');
+    // } else {
+    //     header.classList.remove('sticky');
+    // }
     prevScrollpos = currentScrollPos;
 }
+// var count = document.getElementById('count');
+// var obj = 10650;
+// var cur = parseInt(count.innerHTML);
+
+
+// setInterval(() => {
+//     if (cur <= obj) {
+//         cur = cur + 156;
+//         count.innerHTML = cur;
+//     }
+// }, 100);
+var currentJobsNumber = 0;
+var finalJobsNumber = 10650;
+function increaseJobs() {
+    let currentJobsNumberText = currentJobsNumber.toString();
+
+    if (currentJobsNumberText.length > 3) {
+        let a = "";
+        let b = "";
+        for (var i = currentJobsNumberText.length - 1; i > currentJobsNumberText.length - 1 - 3; i--)
+            a = currentJobsNumberText[i] + a;
+        for (var i = currentJobsNumberText.length - 1 - 3; i >= 0; i--)
+            b = currentJobsNumberText[i] + b;
+        currentJobsNumberText = b + "," + a;
+    }
+    $("#count").html(currentJobsNumberText);
+    if (currentJobsNumber >= finalJobsNumber)
+        return;
+    if (currentJobsNumber + 72 <= finalJobsNumber) {
+        currentJobsNumber += 72;
+        setTimeout(() => {
+            increaseJobs();
+        }, 30);
+    }
+    else {
+        currentJobsNumber += 1;
+        setTimeout(() => {
+            increaseJobs();
+        }, 20);
+    }
+}
+increaseJobs();
